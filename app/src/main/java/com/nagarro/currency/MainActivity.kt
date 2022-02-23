a48be3957de0ed28b8dd00010851e6c9b7d7f220
Vishal Haridas <vishalharidasofficial@gmail.com>
2022-02-23 18:17:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/nagarro/currency/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nagarro.currency
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nagarro.currency.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
